93d7baedbc9a4f3a6027d72dbe299e1d3af34eda
Luke <93022030+tanluc1e@users.noreply.github.com>
2023-03-25 19:09:18 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,6 +21,13 @@ mongoose
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://ludustore.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
