4790523bb51a5872f3b6c6206f127ea5282ef061
Luke <93022030+tanluc1e@users.noreply.github.com>
2023-03-25 19:09:28 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -43,12 +43,6 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
