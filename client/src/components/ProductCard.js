6c64856554c63393754af0ee70db679811ecc97b
Luke <93022030+tanluc1e@users.noreply.github.com>
2023-03-26 14:32:29 +0700

    Update ProductCard.js

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -62,7 +62,7 @@ const ProductCard = ({
     const fetchData = async () => {
       dispatch({ type: "FETCH_REQUEST" });
       try {
-        const result = await axios.get("/api/products");
+        const result = await axios.get("https://ludustore2.onrender.com/api/products");
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
